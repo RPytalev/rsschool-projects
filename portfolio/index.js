9e3903536f36e42085a21d2559dcd10aebe8bc7a
Roman Pytalev <rpytalev@gmail.com>
2022-01-20 07:10:02 +0300

    feat: implement function getTranslate and refactoring function changeClassActive

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,15 @@
 'use strict';
+import i18Obj from './translate.js';
+
 console.log("1.Смена изображений в секции portfolio +25\n2.Перевод страницы на два языка +25\n3.Переключение светлой и тёмной темы +25\n4.Дополнительный функционал: выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы +5\n5.Дополнительный функционал: сложные эффекты для кнопок при наведении и/или клике +5\nИтог: 85");
 
+const portfolioBtns = document.querySelectorAll('.portfolio-btn');
+const portfolioImages = document.querySelectorAll('.gallery-img');
+const portfolioBtnsGroup = document.querySelector('.portfolio-btn-group');
+const ru = document.querySelector('.ru');
+const en = document.querySelector('.en');
+const text = document.querySelectorAll('[data-i18]');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 let hamb = document.querySelector('.hamburger');
 let hambMenu = document.querySelector('.hamburger-menu');
 let hambLinkTop = document.querySelector('.hamburger-item:nth-of-type(1)');
@@ -26,31 +35,40 @@ function closeMenu(event) {
     }
 }
 
-const portfolioBtns = document.querySelectorAll('.portfolio-btn');
-const portfolioImages = document.querySelectorAll('.gallery-img');
-const portfolioBtnsGroup = document.querySelector('.portfolio-btn-group');
-
 portfolioBtnsGroup.onclick = function(event) {
-    let target = event.target;
-    changeImage(target);
+    let targetBtns = event.target;
+    changeImage(targetBtns);
 }
 
-function changeImage(target) {
-    if (target.dataset.season) {
-        portfolioImages.forEach((img, index) => img.src = `./assets/img/${target.dataset.season}/${index + 1}.jpg`);
-        changeClassActive(target);
+function changeImage(targetBtns) {
+    if (targetBtns.dataset.season) {
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/${targetBtns.dataset.season}/${index + 1}.jpg`);
+        changeClassActive(targetBtns);
     }
 }
 
 function changeClassActive(target) {
-    let selectedBtn = target;
-    portfolioBtns.forEach(element => {
-        element.classList.remove('portfolio-btn.active');
-    });
-    selectedBtn.classList.add('portfolio-btn.active');
+    console.log(target.dataset);
+    if (target.dataset.season) {
+        let selectedBtn = target.dataset.season;
+        portfolioBtns.forEach(element => {
+            if (element.dataset.season == selectedBtn) {
+                element.classList.add('activeBtn')
+            } else {
+                element.classList.remove('activeBtn');
+            }
+        });
+    } else {
+        let selectedLang = target.dataset.lang;
+        if (selectedLang == 'ru') {
+            en.classList.remove('activeLink');
+            ru.classList.toggle('activeLink');
+        } else {
+            ru.classList.remove('activeLink');
+            en.classList.toggle('activeLink');
+        }
+    }
 }
-
-const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
 function preloadImages() {
     seasons.forEach(element => {
@@ -61,3 +79,25 @@ function preloadImages() {
     });
 }
 preloadImages();
+
+en.onclick = function(event) {
+    let targetToggle = event.target;
+    getTranslate(targetToggle);
+}
+
+ru.onclick = function(event) {
+    let targetToggle = event.target;
+    getTranslate(targetToggle);
+}
+
+function getTranslate(targetToggle) {
+    let lang = targetToggle.dataset.lang;
+    text.forEach(element => {
+        if (lang == 'ru') {
+            element.textContent = i18Obj.ru[element.dataset.i18];
+        } else {
+            element.textContent = i18Obj.en[element.dataset.i18];
+        }
+    });
+    changeClassActive(targetToggle);
+}
